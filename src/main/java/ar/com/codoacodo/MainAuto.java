c54b5e537f75c7e621d25693ec2ab55d451c7b7c
CloRevollo <clo.revollo@gmail.com>
2023-12-22 21:58:34 -0300

    first commit: esqueleto

---
+++ src/main/java/ar/com/codoacodo/MainAuto.java
@@ -0,0 +1,21 @@
+package ar.com.codoacodo;
+
+public class MainAuto {
+
+		public static void main(String[] args) {
+			//instanciar un objeto de la clase Auto
+			//crear un auto a partir de la clase auto, con NEW, nace un objeto
+			Auto clio = new Auto(
+					"renault", 
+					"clio", 
+					"azul", 
+					2020, 
+					150f);
+			//clio es un objeto de la Clase Auto
+			
+			clio.encender();
+			clio.acelerar();
+			clio.acelerar();
+			clio.frenarConGanas();
+		}
+}
